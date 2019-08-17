8e81b2a89bf3f5d601309392851965d7fefcf78e
hugues <ghugues@netc.fr>
2019-08-17 16:20:38 +0200

    v0.1 ++ dev

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// Mudflap
+// 2019 GUILLEUS Hugues <ghugues@netc.fr>
+// BSD 3-Clause "New" or "Revised" License
+
+import * as y from "./yolo.js" ;
+
+console.log(y.yolo)
+
+console.log(browser.pageAction);
---
+++ pages/yolo.js
@@ -0,0 +1,5 @@
+// Mudflap
+// 2019 GUILLEUS Hugues <ghugues@netc.fr>
+// BSD 3-Clause "New" or "Revised" License
+
+export const yolo = "YOLO !!!";
